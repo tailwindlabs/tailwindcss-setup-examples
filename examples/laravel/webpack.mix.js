d69473111baa8ad062acf356c0fbfe6485b3dd39
mtvbrianking <mtvbrianking@gmail.com>
2019-06-05 23:33:28 +0300

    Compile tailwind css.

--- examples/laravel/webpack.mix.js
+++ examples/laravel/webpack.mix.js
@@ -11,5 +11,21 @@ const mix = require('laravel-mix');
  |
  */
 
+let tailwindcss = require('tailwindcss');
+
+mix.js('resources/js/app.js', 'public/js');
+mix.postCss('resources/css/tailwind.css', 'public/css/app.css', [
+        require('tailwindcss'),
+    ]);
+
+/*
+ * Use tailwind as SASS
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/tailwind.scss', 'public/css/app.css')
+    .options({
+        processCssUrls: false,
+        postCss: [
+            tailwindcss('./tailwind.config.js')
+        ],
+    });
+*/
