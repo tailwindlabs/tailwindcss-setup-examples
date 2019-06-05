17bcc062d45fd62eabe35f8b9250ba60cb74d336
mtvbrianking <mtvbrianking@gmail.com>
2019-06-06 00:14:15 +0300

    Rewrite welcome view.

--- examples/laravel/resources/views/welcome.blade.php
+++ examples/laravel/resources/views/welcome.blade.php
@@ -6,92 +6,41 @@
 
         <title>Laravel</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body>
-        <div class="flex-center position-ref full-height">
+    <body class="bg-white text-gray-600 font-sans font-thin h-screen m-0">
+        <div class="flex items-center justify-end h-12 no-underline text-sm font-normal">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a class="pr-5" href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a class="pr-5" href="{{ route('login') }}">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a class="pr-5" href="{{ route('register') }}">Register</a>
                         @endif
                     @endauth
                 </div>
             @endif
-
-            <div class="content">
-                <div class="title m-b-md">
+        </div>
+        <div class="flex items-center justify-center h-screen -mt-12">
+            <div class="text-center">
+                <div class="text-6xl mb-6">
                     Laravel
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="uppercase no-underline text-sm font-normal">
+                    <a class="" href="https://laravel.com/docs">Docs</a>
+                    <a class="px-8" href="https://laracasts.com">Laracasts</a>
+                    <a class="px-8" href="https://laravel-news.com">News</a>
+                    <a class="px-8" href="https://blog.laravel.com">Blog</a>
+                    <a class="px-8" href="https://nova.laravel.com">Nova</a>
+                    <a class="px-8" href="https://forge.laravel.com">Forge</a>
+                    <a class="px-8" href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
