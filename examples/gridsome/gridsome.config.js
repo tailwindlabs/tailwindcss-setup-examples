6b69979284c310d8c9c7be49c948378821691bb4
Brandon Pittman <gitlab@brandonpittman.net>
2019-06-06 22:25:41 +0900

    Use plugin

--- examples/gridsome/gridsome.config.js
+++ examples/gridsome/gridsome.config.js
@@ -1,20 +1,9 @@
-const purgecss = require('@fullhuman/postcss-purgecss')
-const tailwind = require('tailwindcss')
-
-const postcssPlugins = [
-  tailwind('./tailwind.config.js'),
-]
-
-// add purgecss only when building for production
-if (process.env.NODE_ENV === 'production') postcssPlugins.push(purgecss())
-
 module.exports = {
   siteName: 'Gridsome Tailwind Setup',
-  css: {
-    loaderOptions: {
-      postcss: {
-        plugins: postcssPlugins,
-      },
+  plugins: [
+    {
+      use: 'gridsome-plugin-tailwindcss',
     },
-  },
+  ],
 }
+
