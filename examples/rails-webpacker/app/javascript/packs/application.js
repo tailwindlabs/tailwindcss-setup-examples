c3cfc100af3fc8cbe03b75fb463b1dc17735bd0c
Guillaume Briday <guillaumebriday@gmail.com>
2019-06-06 23:20:41 +0200

    Adding tailwind configuration

--- examples/rails-webpacker/app/javascript/packs/application.js
+++ examples/rails-webpacker/app/javascript/packs/application.js
@@ -15,3 +15,5 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import '../src/tailwind.css'
--- examples/rails-webpacker/postcss.config.js
+++ examples/rails-webpacker/postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('tailwindcss'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
