12b926acec7738e3374d2b90899809a3293c724f
Sébastien Chopin <seb@nuxtjs.com>
2019-06-06 11:49:02 +0200

    add: about page and rename public to static

--- examples/nuxt/pages/index.vue
+++ examples/nuxt/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="min-h-screen flex items-center justify-center">
-    <h1 class="text-5xl font-bold text-teal-600">Hello from Tailwind.</h1>
+  <div class="min-h-screen flex flex-col items-center justify-center">
+    <h1 class="text-5xl font-bold text-teal-300">Hello from Tailwind.</h1>
+    <n-link to="/about">About page</n-link>
   </div>
 </template>
