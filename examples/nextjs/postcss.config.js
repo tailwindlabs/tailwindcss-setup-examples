f001b4f6844e0810609e1437a0d0f3229e6686a1
Marko <crashxx@gmail.com>
2019-08-14 00:21:22 +0200

    Fix purgecss & add more relaistic use case
    Major problem was with purgecss not working, I likned everything
    correctly & added cssnano as well few example with how doese it work
    with included css file, as well how to ignore css with purge.
    
    As bonus, I set up `now` config, and rearanged package.json to depict
    what I think is ok.
    
    You should correct README, as ENG is not my main language.

--- examples/nextjs/postcss.config.js
+++ examples/nextjs/postcss.config.js
@@ -1,12 +1,33 @@
+// Minify css
+const cssnano = require('cssnano')
+
+// Remove what's not need from tailwind
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./public/**/*.html'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  content: ['./components/**/*.jsx', './pages/**/*.js'],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ]
+// import tailwindcss
+const tailwind = require('tailwindcss')('./tailwind.config.js')
+
+// to inline @import rules content with extra features
+const postcssImport = require('postcss-import')
+
+// add vendor prefixes
+const autoprefixer = require('autoprefixer')
+
+// common plugins for both dev & production env
+let plugins = [postcssImport, tailwind, autoprefixer]
+
+// add only when production
+if (process.env.NODE_ENV === 'production') {
+  plugins = plugins.concat([
+    purgecss,
+    cssnano({
+      preset: 'default',
+    }),
+  ])
 }
+
+// export plugins
+module.exports = { plugins }
