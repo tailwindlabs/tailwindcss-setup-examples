e2ef32eee954b7ab2d9093515cad176d2612b90b
Adam Wathan <adam.wathan@gmail.com>
2019-11-07 08:06:42 -0500

    Add purgecss to Next.js example

--- examples/nextjs/postcss.config.js
+++ examples/nextjs/postcss.config.js
@@ -1,6 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
-  content: ['./public/**/*.html'],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  content: ['./components/**/*.js', './pages/**/*.js'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
 })
 
 module.exports = {
@@ -8,5 +8,6 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss'),
     require('autoprefixer'),
-  ]
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : []),
+  ],
 }
