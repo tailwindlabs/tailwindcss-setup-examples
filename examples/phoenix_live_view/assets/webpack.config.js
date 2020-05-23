be554d1590ca35e78ab16e3e769ad9e2e52c8d74
Adam Lancaster <lancaster28@hotmail.com>
2020-05-23 17:11:09 +0100

    Add tailwind to phoenix example

--- examples/phoenix_live_view/assets/webpack.config.js
+++ examples/phoenix_live_view/assets/webpack.config.js
@@ -39,6 +39,7 @@ module.exports = (env, options) => {
             MiniCssExtractPlugin.loader,
             'css-loader',
             'sass-loader',
+            'postcss-loader'
           ],
         }
       ]
