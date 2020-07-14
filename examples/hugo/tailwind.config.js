a7e0e25112617210bbe4ed8fd0df6581c6283bbc
praveenjuge <praveen@skcript.com>
2020-07-14 11:05:43 +0530

    Added Hugo Example
    Signed-off-by: praveenjuge <praveen@skcript.com>

---
+++ examples/hugo/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  purge: {
+    content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
