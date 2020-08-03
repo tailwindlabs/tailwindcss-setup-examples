5b15cea84a866c1eb23fa55ff72ccfc69e89d718
Akshat Sharma <er.sharmaakshat@gmail.com>
2020-08-03 12:10:03 +0400

    chore: added SASS example

--- examples/nextjs/pages/index.tsx
+++ examples/nextjs/pages/index.tsx
@@ -2,7 +2,7 @@ import Layout from "../components/Layout";
 
 const Home = () => (
   <Layout>
-    <h1 className="text-3xl font-bold text-purple-500">Hello world!</h1>
+    <h1 className="text-3xl font-bold">Hello world!</h1>
   </Layout>
 );
 
