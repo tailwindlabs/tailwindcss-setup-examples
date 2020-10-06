2dd8b348002881e0ee05bb382a269c5689e6e3bc
Kelvin Omereshone <kelvinomereshone@gmail.com>
2020-10-06 11:39:23 +0100

    chore: initialize the sails app

---
+++ examples/sails/config/custom.js
@@ -0,0 +1,22 @@
+/**
+ * Custom configuration
+ * (sails.config.custom)
+ *
+ * One-off settings specific to your application.
+ *
+ * For more information on custom configuration, visit:
+ * https://sailsjs.com/config/custom
+ */
+
+module.exports.custom = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Any other custom config this Sails app should use during development.    *
+  *                                                                          *
+  ***************************************************************************/
+  // sendgridSecret: 'SG.fake.3e0Bn0qSQVnwb1E4qNPz9JZP5vLZYqjh7sn8S93oSHU',
+  // stripeSecret: 'sk_test_Zzd814nldl91104qor5911gjald',
+  // …
+
+};
