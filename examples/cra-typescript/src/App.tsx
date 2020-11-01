ad3b4fcbd206f9f6138d858852db2ac8fa0dadc4
abimaelandrade <abimaelc.andrade@gmail.com>
2020-11-01 11:54:29 -0300

    feat: a example tailwindCSS with CRA

---
+++ examples/cra-typescript/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => (
+  <div>
+    <header>
+      <h1>TailwindCSS with CRA and TypeScript</h1>
+      <a
+        href="https://tailwindcss.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn TailwindCSS
+      </a>
+    </header>
+  </div>
+)
+
+export default App;
